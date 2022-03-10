0f622cfd30e4d260c486a210294e3f736231d024
Nujsvart <kampfar@gmail.com>
2022-03-10 22:32:26 +0300

    countup added

--- 02-time-tracking-dashboard/src/components/CardInfoTotal.js
+++ 02-time-tracking-dashboard/src/components/CardInfoTotal.js
@@ -1,9 +1,19 @@
 import React from "react";
+import CountUp from "react-countup";
 
 const CardInfoTotal = ({ times, lastTime }) => {
   return (
     <>
-      <p className="current">{times.current}hrs</p>
+      <CountUp
+        start={0}
+        end={times.current}
+        delay={0}
+        duration={1}
+        suffix="hrs"
+      >
+        {({ countUpRef }) => <p ref={countUpRef} className="current" />}
+      </CountUp>
+
       <p className="previous">
         {`Last ${lastTime}`} - {times.previous}hrs
       </p>
